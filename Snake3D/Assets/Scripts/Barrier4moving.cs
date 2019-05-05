ffd39b4763a21a404734be5fcc0ed8c020067f5b
Никита <kharlanchuk.nikita@mail.ru>
2019-05-05 18:11:53 +0200

    Исправил недоработки

---
+++ Snake3D/Assets/Scripts/Barrier4moving.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrier4moving : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+        if (transform.position.x > 14)
+        {
+            speed = -1f;
+        }
+        if (transform.position.x < 10.4)
+        {
+            speed = 1f;
+        }
+    }
+
+        
+  
+}
