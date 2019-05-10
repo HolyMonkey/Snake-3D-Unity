3fcecef86c366e95569cc5c358c4baf48a41d254
Алексей Алексей <shimaleksej@gmail.com>
2019-05-10 18:47:42 +0300

    added new level

---
+++ Snake3D/Assets/Scripts/MoveFood.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFood : MonoBehaviour
+{
+    public float speed = 5;
+    public Transform[] moveSpots;
+    private int randomSpots;
+    
+
+
+    void Start()
+    {
+        randomSpots = Random.Range(0, moveSpots.Length);
+    }
+
+    
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpots].position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, moveSpots[randomSpots].position) < 0.2f)        
+            randomSpots = Random.Range(0, moveSpots.Length);
+        
+    }
+
+}
