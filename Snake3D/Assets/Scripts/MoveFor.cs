42442e3e4ca5465873591be700c830c7a2a3f308
Алексей Алексей <shimaleksej@gmail.com>
2019-05-14 20:22:44 +0300

    012 re-named level, re-named variable

--- Snake3D/Assets/Scripts/MoveFor.cs
+++ Snake3D/Assets/Scripts/MoveFor.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class MoveFor : MonoBehaviour
 {
     [SerializeField] private float _speed;    
-    [SerializeField] private Transform[] spots;
+    [SerializeField] private Transform[] _spots;
     private int _currentSpot;
 
     private void Start()
     {
-        _currentSpot = Random.Range(0, spots.Length);
+        _currentSpot = Random.Range(0, _spots.Length);
     }
 
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, spots[_currentSpot].position, _speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, _spots[_currentSpot].position, _speed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, spots[_currentSpot].position) < 0.2f)
-            _currentSpot = Random.Range(0, spots.Length);
+        if (Vector3.Distance(transform.position, _spots[_currentSpot].position) < 0.2f)
+            _currentSpot = Random.Range(0, _spots.Length);
     }
 }
