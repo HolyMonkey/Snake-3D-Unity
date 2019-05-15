9280e95bee9469b605945c344dcaada7b0e1b07e
Pavel Osminin <pav.osminin@gmail.com>
2019-05-15 23:22:34 +0500

    RemoveBone() method added

--- Snake3D/Assets/Scripts/SnakeControllerMatch3.cs
+++ Snake3D/Assets/Scripts/SnakeControllerMatch3.cs
@@ -33,20 +33,26 @@ public class SnakeControllerMatch3 : SnakeController
                 }
                 if (Tails.Count >= 3)
                 {
-                    for (int j = 0; j < Tails.Count - 2; j++)
-                    {
-                        if (Equals(Tails[j].GetComponent<Renderer>().material.color, Tails[j + 1].GetComponent<Renderer>().material.color) &&
-                            Equals(Tails[j].GetComponent<Renderer>().material.color, Tails[j + 2].GetComponent<Renderer>().material.color))
-                        {
-                            Destroy(Tails[j + 2].gameObject);
-                            Destroy(Tails[j + 1].gameObject);
-                            Destroy(Tails[j].gameObject);
-                            Tails.RemoveAt(j + 2);
-                            Tails.RemoveAt(j + 1);
-                            Tails.RemoveAt(j);
-                        }
-                    }
+                    RemoveBone();
                 }
         }             
+    }
+
+    private void RemoveBone()
+    {
+        for (int j = 0; j < Tails.Count - 2; j++)
+        {
+            var currentTailColor = Tails[j].GetComponent<Renderer>().material.color;
+            if (Equals(currentTailColor, Tails[j + 1].GetComponent<Renderer>().material.color) &&
+                Equals(currentTailColor, Tails[j + 2].GetComponent<Renderer>().material.color))
+            {
+                Destroy(Tails[j + 2].gameObject);
+                Destroy(Tails[j + 1].gameObject);
+                Destroy(Tails[j].gameObject);
+                Tails.RemoveAt(j + 2);
+                Tails.RemoveAt(j + 1);
+                Tails.RemoveAt(j);
+            }
+        }
     }
 }
