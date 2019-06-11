bf1089d92bea8c4cc2b28bd5e0c5e35784fbe6af
Denis Savich <deonisiu@ya.ru>
2019-06-12 00:56:40 +0700

    feature(new level) added apmlitude movement

--- Snake3D/Assets/Scripts/SnakeController.cs
+++ Snake3D/Assets/Scripts/SnakeController.cs
@@ -12,23 +12,48 @@ public class SnakeController : MonoBehaviour
     public GameObject BonePrefab;
     [Range(0,4)]
     public float Speed;
+	[Range(1.01f, 1.1f)]
+	public float Acceleration;
     [Range(4, 8)]
     public float rotationSpeed;
-    public GameObject Bone;
+	[Range(0, 6)]
+	public float AmplitudeMaximum;
+	public bool isAmplitudeMove = false;
+	public GameObject Bone;
     public UnityEvent OnEat;
     private Transform _transform;
     private Ghost _isGhost;
-    
-    private void Start()
+	private float _amplitude = 3f;
+	private float _oneFifteenth;
+	private bool _direction = false;
+
+	private void Start()
     {
-        _transform = GetComponent<Transform>();
+		_oneFifteenth = AmplitudeMaximum / 15f;
+		_transform = GetComponent<Transform>();
         _isGhost = GetComponent<Ghost>();
     }
 
-    private void Update()
+	private void changeAmplitude()
+	{
+		_amplitude += _direction ? _oneFifteenth : _oneFifteenth * -1f;
+		if(_amplitude >= AmplitudeMaximum) {
+			_direction = false;
+		}
+		if(_amplitude <= (AmplitudeMaximum * -1)) {
+			_direction = true;
+		}
+	}
+
+	private void MoveAmplitude() {
+		_transform.Translate(new Vector3(_amplitude, 0, 0) * Time.deltaTime * Tails.Count / 2);
+		changeAmplitude();
+	}
+
+	private void Update()
     {
         MoveSnake(_transform.position + _transform.forward * Speed);
-        float angle = Input.GetAxis("Horizontal") * rotationSpeed;
+		float angle = Input.GetAxis("Horizontal") * rotationSpeed;
         _transform.Rotate(0, angle, 0);
     }   
 
@@ -53,9 +78,13 @@ public class SnakeController : MonoBehaviour
 
 
         _transform.position = newPosition;
-    }
 
-    protected void OnHitBarrier()
+		if(isAmplitudeMove) {
+			MoveAmplitude();
+		}
+	}
+
+	protected void OnHitBarrier()
     {
         if (Tails.Count >= 2)
         {
@@ -93,7 +122,7 @@ public class SnakeController : MonoBehaviour
             Destroy(collision.gameObject);
             var bone = Instantiate(BonePrefab);
             Tails.Add(bone.transform);
-            Speed *= 1.1f;
+            Speed *= Acceleration;
             if (OnEat != null)
             {
                 OnEat.Invoke();
