dfd965ff9452c56bc0176c0875d8b4df4f99fe21
Denis Savich <deonisiu@ya.ru>
2019-06-12 00:55:00 +0700

    feature(new level) add countdown for Food

--- Snake3D/Assets/Scripts/SpawnFood.cs
+++ Snake3D/Assets/Scripts/SpawnFood.cs
@@ -7,16 +7,37 @@ public class SpawnFood : MonoBehaviour
     public GameObject PrefabFood;
     public Transform ObjectTopRightBorder, ObjectBotLeftBorder;
     private GameObject _cloneFood;
+	private Renderer _renderer;
+	private float _alpha;
+	public bool isCountdown = false;
+	[Range(0.001f, 0.1f)]
+	public float CountdownSpeed;
 
     private void Start()
     {
-        SpawnFoodObject();
-    }
+		SpawnFoodObject();
+	}
 
-    public void SpawnFoodObject()
+	public void SpawnFoodObject()
     {
-        _cloneFood = Instantiate(PrefabFood, transform);
-        Vector3 positionClone = new Vector3(Random.Range(ObjectBotLeftBorder.localPosition.x, ObjectTopRightBorder.localPosition.x),1,Random.Range(ObjectBotLeftBorder.localPosition.z, ObjectTopRightBorder.localPosition.z));
-        _cloneFood.transform.localPosition = positionClone;
-    }
+		_alpha = 1f;
+		_cloneFood = Instantiate(PrefabFood, transform);
+		Vector3 positionClone = new Vector3(Random.Range(ObjectBotLeftBorder.localPosition.x, ObjectTopRightBorder.localPosition.x),1,Random.Range(ObjectBotLeftBorder.localPosition.z, ObjectTopRightBorder.localPosition.z));
+		_cloneFood.transform.localPosition = positionClone;
+
+		_renderer = _cloneFood.GetComponent<Renderer>();
+	}
+
+	public void Update() {
+		// Если включен таймер обратного отчета уменьшаем альфа канал до 0
+		if(isCountdown) {
+			if (_alpha > 0) {
+				_renderer.material.color = new Color(0, 1f, 0, _alpha);
+				_alpha -= CountdownSpeed;
+			} else {
+				Destroy(_cloneFood);
+				SpawnFoodObject();
+			}
+		}
+	}
 }
