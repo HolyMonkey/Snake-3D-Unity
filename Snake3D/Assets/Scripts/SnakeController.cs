305516c740126d369a01577404295a59c1833d96
Denis Savich <deonisiu@ya.ru>
2019-06-15 10:00:15 +0700

    style(tabs) tabs to spaces

--- Snake3D/Assets/Scripts/SnakeController.cs
+++ Snake3D/Assets/Scripts/SnakeController.cs
@@ -12,50 +12,50 @@ public class SnakeController : MonoBehaviour
     public GameObject BonePrefab;
     [Range(0,4)]
     public float Speed;
-	[Range(1.01f, 1.1f)]
-	public float Acceleration;
+    [Range(1.01f, 1.1f)]
+    public float Acceleration;
     [Range(4, 8)]
     public float rotationSpeed;
-	[Range(0, 6)]
-	public float AmplitudeMaximum;
-	public bool isAmplitudeMove = false;
-	public GameObject Bone;
+    [Range(0, 6)]
+    public float AmplitudeMaximum;
+    public bool isAmplitudeMove = false;
+    public GameObject Bone;
     public UnityEvent OnEat;
     private Transform _transform;
     private Ghost _isGhost;
-	private float _amplitude = 3f;
-	private float _oneFifteenth;
-	private bool _direction = false;
+    private float _amplitude = 3f;
+    private float _oneFifteenth;
+    private bool _direction = false;
 
 	private void Start()
     {
-		_oneFifteenth = AmplitudeMaximum / 15f;
-		_transform = GetComponent<Transform>();
+        _oneFifteenth = AmplitudeMaximum / 15f;
+        _transform = GetComponent<Transform>();
         _isGhost = GetComponent<Ghost>();
     }
 
-	private void changeAmplitude()
-	{
-		_amplitude += _direction ? _oneFifteenth : _oneFifteenth * -1f;
-		if(_amplitude >= AmplitudeMaximum) {
-			_direction = false;
-		}
-		if(_amplitude <= (AmplitudeMaximum * -1)) {
-			_direction = true;
-		}
-	}
+    private void changeAmplitude()
+    {
+        _amplitude += _direction ? _oneFifteenth : _oneFifteenth * -1f;
+        if(_amplitude >= AmplitudeMaximum) {
+            _direction = false;
+        }
+        if(_amplitude <= (AmplitudeMaximum * -1)) {
+            _direction = true;
+        }
+    }
 
-	private void MoveAmplitude() {
-		_transform.Translate(new Vector3(_amplitude, 0, 0) * Time.deltaTime * Tails.Count / 2);
-		changeAmplitude();
-	}
+    private void MoveAmplitude() {
+        _transform.Translate(new Vector3(_amplitude, 0, 0) * Time.deltaTime * Tails.Count / 2);
+        changeAmplitude();
+    }
 
 	private void Update()
     {
         MoveSnake(_transform.position + _transform.forward * Speed);
-		float angle = Input.GetAxis("Horizontal") * rotationSpeed;
+        float angle = Input.GetAxis("Horizontal") * rotationSpeed;
         _transform.Rotate(0, angle, 0);
-    }   
+    }
 
     private void MoveSnake(Vector3 newPosition)
     {
@@ -79,12 +79,12 @@ public class SnakeController : MonoBehaviour
 
         _transform.position = newPosition;
 
-		if(isAmplitudeMove) {
-			MoveAmplitude();
-		}
-	}
+        if(isAmplitudeMove) {
+            MoveAmplitude();
+        }
+    }
 
-	protected void OnHitBarrier()
+    protected void OnHitBarrier()
     {
         if (Tails.Count >= 2)
         {
@@ -127,6 +127,6 @@ public class SnakeController : MonoBehaviour
             {
                 OnEat.Invoke();
             }
-        }             
+        }
     }
 }
--- Snake3D/Assets/Scripts/SpawnFood.cs
+++ Snake3D/Assets/Scripts/SpawnFood.cs
@@ -7,36 +7,36 @@ public class SpawnFood : MonoBehaviour
     public GameObject PrefabFood;
     public Transform ObjectTopRightBorder, ObjectBotLeftBorder;
     private GameObject _cloneFood;
-	private Renderer _renderer;
-	private float _alpha;
-	public bool isCountdown = false;
-	[Range(0.001f, 0.1f)]
-	public float CountdownSpeed;
+    private Renderer _renderer;
+    private float _alpha;
+    public bool isCountdown = false;
+    [Range(0.001f, 0.1f)]
+    public float CountdownSpeed;
 
     private void Start()
     {
-		SpawnFoodObject();
-	}
+        SpawnFoodObject();
+    }
 
-	public void SpawnFoodObject()
+    public void SpawnFoodObject()
     {
-		_alpha = 1f;
-		_cloneFood = Instantiate(PrefabFood, transform);
-		Vector3 positionClone = new Vector3(Random.Range(ObjectBotLeftBorder.localPosition.x, ObjectTopRightBorder.localPosition.x),1,Random.Range(ObjectBotLeftBorder.localPosition.z, ObjectTopRightBorder.localPosition.z));
-		_cloneFood.transform.localPosition = positionClone;
+        _alpha = 1f;
+        _cloneFood = Instantiate(PrefabFood, transform);
+        Vector3 positionClone = new Vector3(Random.Range(ObjectBotLeftBorder.localPosition.x, ObjectTopRightBorder.localPosition.x),1,Random.Range(ObjectBotLeftBorder.localPosition.z, ObjectTopRightBorder.localPosition.z));
+        _cloneFood.transform.localPosition = positionClone;
 
-		_renderer = _cloneFood.GetComponent<Renderer>();
-	}
+        _renderer = _cloneFood.GetComponent<Renderer>();
+    }
 
-	public void Update() {
-		if(isCountdown) {
-			if (_alpha > 0) {
-				_renderer.material.color = new Color(0, 1f, 0, _alpha);
-				_alpha -= CountdownSpeed;
-			} else {
-				Destroy(_cloneFood);
-				SpawnFoodObject();
-			}
-		}
-	}
+    public void Update() {
+        if(isCountdown) {
+            if (_alpha > 0) {
+                _renderer.material.color = new Color(0, 1f, 0, _alpha);
+                _alpha -= CountdownSpeed;
+            } else {
+                Destroy(_cloneFood);
+                SpawnFoodObject();
+            }
+        }
+    }
 }
